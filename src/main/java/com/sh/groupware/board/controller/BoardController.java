11ee1691ce7a5c21f2bdffa94c9f5f8f0e46951d
93kimkh <93rlgnsl@gmail.com>
2023-03-22 11:19:42 +0900

    기훈 임시커밋

--- src/main/java/com/sh/groupware/board/controller/BoardController.java
+++ src/main/java/com/sh/groupware/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.sh.groupware.board.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -10,11 +12,16 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sh.groupware.board.model.dto.Board;
 import com.sh.groupware.board.model.service.BoardService;
+import com.sh.groupware.common.HelloSpringUtils;
+import com.sh.groupware.common.dto.Attachment;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -57,6 +64,52 @@ public class BoardController {
 //		model.addAttribute("board", board);
 //		
 //	}
+	
+	@PostMapping("/boardEnroll.do")
+	public String boardEnroll(
+			Board board, 
+			@RequestParam("upFile") List<MultipartFile> upFiles, 
+			RedirectAttributes redirectAttr) {
+		
+		
+		// ServletContext : application객체의 타입. DI. 스프링과 관계없는 servlet spec의 객체
+		String saveDirectory = application.getRealPath("/resources/upload/board");
+		log.debug("saveDirectory = {}", saveDirectory);
+		
+		// 첨부파일 저장(서버컴퓨터) 및 Attachment객체 만들기
+		for(MultipartFile upFile : upFiles) {
+			log.debug("upFile = {}", upFile);
+//			log.debug("upFile = {}", upFile.getOriginalFilename());
+//			log.debug("upFile = {}", upFile.getSize());	
+			
+			if(upFile.getSize() > 0) {
+				// 1. 저장 
+				String renameFilename = HelloSpringUtils.renameMultipartFile(upFile);
+				String originalFilename = upFile.getOriginalFilename();
+				File destFile = new File(saveDirectory, renameFilename);
+				try {
+					upFile.transferTo(destFile);
+				} catch (IllegalStateException | IOException e) {
+					log.error(e.getMessage(), e);
+				}
+				
+				// 2. attach객체생성 및 Board에 추가
+				Attachment attach = new Attachment();
+				attach.setRenameFilename(renameFilename);
+				attach.setOriginalFilename(originalFilename);
+				board.addAttachment(attach);
+			}
+			
+			
+		}
+		
+		log.debug("board = {}", board);
+		int result = boardService.insertBoard(board);
+		
+		redirectAttr.addFlashAttribute("msg", "게시글을 성공적으로 저장했습니다.");
+		
+		return "redirect:/board/boardList.do";
+	}
 	
 	
 	
